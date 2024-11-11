a4bcd7db877086da9f21bbbde787ea4ccc73fd40
ANDRINE PAULO ALMEIDA VICENTE DA SILVA <andrine.pavsilva@senacsp.edu.br>
2024-11-11 08:44:17 -0300

    atividade python

---
+++ atividade_1.py
@@ -0,0 +1,11 @@
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+if num1 < num2:
+    print(num2)
+
+else:
+    print(num1)
+
+if num1 == num2:
+    print("números iguais")
---
+++ atividade_3.py
@@ -0,0 +1,10 @@
+sexo = input("Digite F para feminino e M para masculino: ")
+
+if sexo =="F" or sexo =="f":
+    print("Feminino")
+elif sexo =="M" or sexo =="m":
+    print("Masculino")
+else:
+    print("Sexo invalido")
+
+    
