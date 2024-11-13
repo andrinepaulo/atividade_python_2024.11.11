516e9cd756b480667a30aefefd04ef4c46d6165a
ANDRINE PAULO ALMEIDA VICENTE DA SILVA <andrine.pavsilva@senacsp.edu.br>
2024-11-13 08:56:21 -0300

    subindo arquivos

---
+++ modulo_funcoes.py
@@ -0,0 +1,21 @@
+def saudacao(nome=""):
+    print(f"Olá, {nome}! Seja Bem Vindo")
+
+def area_retangulo(base=0, altura=0):
+    area = base * altura
+    return area
+
+def potencia(base, expoente):
+    resultado = base ** expoente
+    return
+
+def soma(*numeros):
+    resultado = 0
+    for i in numeros:
+        resultado += 1
+    return resultado
+
+def soma2(a,b):
+    return a+b
+
+
